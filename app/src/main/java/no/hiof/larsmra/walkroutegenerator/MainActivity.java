31e4f74524f5f31f89c5a0bf7c638af39021ab09
larsmra <lars.m.randem@hiof.no>
2020-10-11 21:49:26 +0200

    Cleaned up the code.

--- app/src/main/java/no/hiof/larsmra/walkroutegenerator/MainActivity.java
+++ app/src/main/java/no/hiof/larsmra/walkroutegenerator/MainActivity.java
@@ -1,11 +1,8 @@
 package no.hiof.larsmra.walkroutegenerator;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -26,18 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //FloatingActionButton fab = findViewById(R.id.fab);
-        /*fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_schedule, R.id.nav_friend_list, R.id.nav_group_list,
                 R.id.nav_scoreboard, R.id.nav_profile)
@@ -50,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
@@ -60,23 +46,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        System.out.println("TEST: Resuming");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        System.out.println("TEST: Starting");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        System.out.println("TEST: pause");
     }
 }
